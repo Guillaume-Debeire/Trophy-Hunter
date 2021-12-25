107d1d605edd209f41cc1a54de0514bc0c6f5c41
Guillaume.Debeire <guillaume@debeire.com>
2021-12-25 16:35:19 +0100

    initialisation du projet

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+const Header = () => (
+    <div className='header'>
+        <header>
+            <h1>Trophy Hunter</h1>
+        </header>
+    </div>
+);
+
+export default Header;
